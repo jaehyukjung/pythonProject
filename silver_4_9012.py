10b20cf4ff8af920a8fc5d83900ce0b0e4ffac5b
jaehyukjung <jhdool1594@gmail.com>
2022-08-06 14:09:33 +0900

    [08.06] 백준 9012번 문제

--- silver_4_9012.py
+++ silver_4_9012.py
@@ -1,1 +1,20 @@
-N
+N = int(input())
+
+for _ in range(N):
+    s = input()
+    lst = []
+    ans = True
+    for i in s:
+        if i == '(':
+            lst.append(i)
+        elif i == ')':
+            if not lst:
+                ans = False
+                break
+            else:
+                lst.pop(len(lst)-1)
+    if ans and not lst:
+        print('YES')
+    else:
+        print('NO')
+
