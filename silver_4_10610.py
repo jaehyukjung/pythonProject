2c6cf92e3c10bd3d259d2c44a86cb89353f0e4ac
jaehyukjung <jhdool1594@gmail.com>
2022-08-07 09:58:35 +0900

    [08.07] 백준 10610번 문제

--- silver_4_10610.py
+++ silver_4_10610.py
@@ -1,9 +1,20 @@
 number = input()
 
 lst =[x for x in number]
+sum =0
+for x in number:
+    sum += int(x)
+
+if '0' in lst and sum % 3 == 0:
+    lst.sort(reverse=True)
+    s=''
+    for x in lst:
+       s += x
+    print(s)
+
+else:
+    print(-1)
 
 
-
-for _
 
 
