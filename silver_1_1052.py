3440b305b5a77f62b2b4d799ab7cfe9355a81d30
jaehyukjung <jhdool1594@gmail.com>
2022-08-16 19:48:09 +0900

    [08.14] 백준 1052번 문제

--- silver_1_1052.py
+++ silver_1_1052.py
@@ -1,18 +1,28 @@
-n, m = map(int, input().split())
-
-def max_pow(n):
-    i = 1
-    while n/2 > i:
-        i *= 2
-
-    return i
-
-while m > 0:
-    k = max_pow(n)
-    n -= k
-    m -= 1
-
-print(k-n)
 
 
+n = int(input())
+lst = [input() for _ in range(n)]
 
+cnt_row = 0
+cnt_col = 0
+
+for x in lst:
+    ans = x.split('X')
+    for i in ans:
+        if len(i) >=2:
+            cnt_row += 1
+
+col_lst =[]
+for i in range(n):
+    s =''
+    for j in range(n):
+        s += lst[j][i]
+    col_lst.append(s)
+
+for x in col_lst:
+    ans = x.split('X')
+    for i in ans:
+        if len(i) >=2:
+            cnt_col += 1
+
+print(cnt_row, cnt_col)
