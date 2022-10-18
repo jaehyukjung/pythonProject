40312e938fc9f509cda9be56ed16888e4d8c0638
jaehyukjung <jhdool1594@gmail.com>
2022-10-18 13:54:06 +0900

    [10.18] 백준 2193 문제

--- silver_3_2193.py
+++ silver_3_2193.py
@@ -1,31 +1,11 @@
 n = int(input())
 
-lst = list(int(input()) for _ in range(n))
+i = 0
 
-lst.sort()
+cnt = 0
+for i in range(2**(n-1),2**(n)):
+    s = str(bin(i))[2:]
+    if '11' not in (s):
+        cnt+=1
 
-print(int(round(sum(lst) / len(lst),0)))
-
-print(lst[(round(len(lst)/2))])
-
-
-max_count = 0
-
-count_lst = []
-for i in lst:
-    if lst.count(i) > max_count and (i not in count_lst):
-        count_lst.clear()
-        count_lst.append(i)
-        max_count = lst.count(i)
-
-    elif lst.count(i) == max_count and (i not in count_lst):
-        count_lst.append(i)
-
-
-if len(count_lst) > 1 :
-    count_lst.remove(min(count_lst))
-    print(count_lst[0])
-else:
-    print(count_lst[0])
-
-print(lst[len(lst)-1]-lst[0])
+print(cnt)
