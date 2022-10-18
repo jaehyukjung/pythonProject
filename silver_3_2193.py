cd91b4da8303569cdc18cb1ffea3bf3455983bc7
jaehyukjung <jhdool1594@gmail.com>
2022-10-18 20:30:23 +0900

    [10.18] 백준 2193 문제

--- silver_3_2193.py
+++ silver_3_2193.py
@@ -1,11 +1,18 @@
+## 내가 푼 방식 (피보나치 함수 활용) -> 시간초과,.
+
+def fibo(n):
+    if n == 1 or n ==2:
+        return 1
+    else:
+        return fibo(n-1) + fibo(n-2)
+
 n = int(input())
 
-i = 0
+print(fibo(n))
+## 찾아서 본 풀이 -> 재귀를 쓰지 않고 푸는 방식
 
-cnt = 0
-for i in range(2**(n-1),2**(n)):
-    s = str(bin(i))[2:]
-    if '11' not in (s):
-        cnt+=1
+s = [0, 1, 1]
+for i in range(3, 91):
+  s.append(s[i - 2] + s[i - 1])
 
-print(cnt)
+print(s[n])
