644642d9ffb33197cc7d8534122bd3819a3e768e
jaehyukjung <jhdool1594@gmail.com>
2023-02-03 19:14:30 +0900

    [02.03] 백준 16234번 문제

--- gold_5_16234.py
+++ gold_5_16234.py
@@ -1,59 +1,54 @@
-import sys
 from collections import deque
-def bfs(si,sj):
+def bfs(si,sj,h):
     q = deque()
-    v[si][sj] = True
+    v[si][sj] = h
     q.append((si, sj))
-    v2 =[[False] * N for _ in range(N)]
-    v2[si][sj] = True
+    cnt, total = 1, arr[si][sj]
+    ch_list = []
     while q:
         ci, cj = q.popleft()
-
+        ch_list.append((ci,cj))
         for di, dj in ((-1,0),(1,0),(0,-1),(0,1)):
             ni, nj = ci + di, cj + dj
-            if 0<=ni<N and 0<nj<N and not v[ni][nj] and L <= abs(arr[ci][cj] -arr[ni][nj]) <= R:
-                v[ni][nj] = True
-                v2[ni][nj] = True
+            if 0<=ni<N and 0<=nj<N and not v[ni][nj] and L <= abs(arr[ci][cj] -arr[ni][nj]) <= R:
+                v[ni][nj] = h
                 q.append((ni,nj))
-    cnt = 0
-    total = 0
-    for i in range(N):
-        for j in range(N):
-            if v2[i][j] ==True:
-                total += arr[i][j]
-                cnt +=1
+                total += arr[ni][nj]
+                cnt += 1
+    for i, j in ch_list:
+        arr[i][j] = total//cnt
 
-    for i in range(N):
-        for j in range(N):
-            if v2[i][j] == True:
-                arr[i][j] = total//cnt
 
 
 def check(arr):
     for i in range(N):
         for j in range(N):
-            for di, dj in ((-1,0),(1,0),(0,-1),(0,1)):
+            for di, dj in ((1,0),(0,1)):
                 ni, nj = i + di, j + dj
-                if 0<=ni<N and 0<nj<N and L <= abs(arr[i][j] -arr[ni][nj]) <= R:
-                    return True
+
+                if 0 <= ni<N and 0 <=nj<N:
+                    m = abs(arr[i][j] - arr[ni][nj])
+                    if L <= m <= R:
+                        return True
     return False
 
 if __name__ == "__main__":
     N, L, R = map(int,input().split())
     arr = [list(map(int,input().split())) for _ in range(N)]
     check_pos = True
-    i = 0
-    while(True):
-        v = [[False] * N for _ in range(N)]
-        check_pos = check(arr)
+    ans = 0
+    check_pos = check(arr)
+    while(check_pos):
         if not check_pos:
             break
-
+        h = 1
+        v = [[0] * N for _ in range(N)]
         for i in range(N):
             for j in range(N):
-                if not v[i][j]:
-                    bfs(i,j)
+                if v[i][j] == 0:
+                    bfs(i,j,h)
+                    h += 1
+        ans += 1
+        check_pos = check(arr)
 
-        i+=1
-
-    print(i)
+    print(ans)
