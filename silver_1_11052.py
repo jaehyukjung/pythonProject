2cce66dcc8d1cabe97d92da8aabe7fbfb44ee85c
jaehyukjung <jhdool1594@gmail.com>
2023-02-08 20:56:12 +0900

    [02.08] 백준 11052번 문제

---
+++ silver_1_11052.py
@@ -0,0 +1,11 @@
+if __name__ == "__main__":
+    n = int(input())
+    arr = list(map(int,input().split()))
+    arr.insert(0,0)
+    dp = [0 for _ in range(n+1)]
+    for i in range(1,n+1):
+        for j in range(1,i+1):
+            dp[i] = max(dp[i], dp[i-j] + arr[j])
+
+    print(dp[n])
+
