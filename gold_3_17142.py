7601de4b69c054de1fca2f61f70b00a470828c25
jaehyukjung <jhdool1594@gmail.com>
2023-02-17 22:04:30 +0900

    [02.17] 백준 17142번 문제

--- gold_3_17142.py
+++ gold_3_17142.py
@@ -3,29 +3,33 @@ from collections import deque
 
 
 def bfs(change_list):
-    q = deque(change_list)
+    q = deque()
+    result = 0
     v = [[-1] * N for _ in range(N)]
+
     for i, j in change_list:
         v[i][j] = 0
-    min_ans = 0
+        q.append((i,j))
+
     while q:
         ci, cj = q.popleft()
+
         for di, dj in ((-1, 0), (1, 0), (0, -1), (0, 1)):
             ni, nj = ci + di, cj + dj
+
             if 0 <= ni < N and 0 <= nj < N and v[ni][nj] == -1 and arr[ni][nj] == 0:
                 v[ni][nj] = v[ci][cj] + 1
                 q.append((ni, nj))
-                min_ans = max(min_ans,v[ni][nj])
+                result = max(result,v[ni][nj])
             if 0 <= ni < N and 0 <= nj < N and v[ni][nj] == -1 and arr[ni][nj] == 2:
-                v[ni][nj] = v[ci][cj]
+                v[ni][nj] = v[ci][cj] + 1
                 q.append((ni, nj))
 
-    a = 0
-    for i in range(N):
-        if v[i].count(-1) != arr[i].count(1):
-            return N**2
 
-    return min_ans
+    if list(sum(v,[])).count(-1) != w_cnt:
+        return N**2
+
+    return result
 
 
 if __name__ == "__main__":
@@ -40,17 +44,11 @@ if __name__ == "__main__":
             if arr[i][j] == 2:
                 combi_list.append((i, j))
 
-    combi_q = deque(combinations(combi_list, M))
 
-    ans = []
-    while combi_q:
-        change_list = combi_q.popleft()
-        ans.append(bfs(change_list))
 
-    print(min(ans))
+    ans = N**2
+    for change_list in combinations(combi_list, M):
+        ans = min(ans, bfs(change_list))
 
-n = int(input())
-arr = list(map(int,input().split()))
-m = int(input())
+    print(ans if ans != N**2 else -1)
 
-print(arr.count(m))
