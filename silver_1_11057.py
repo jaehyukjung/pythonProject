fd0677202469d903fcbda2524a1e0875b017667c
jaehyukjung <jhdool1594@gmail.com>
2023-02-18 23:28:10 +0900

    [02.18] 백준 11057번 문제

---
+++ silver_1_11057.py
@@ -0,0 +1,12 @@
+import sys
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    n = int(input())
+    dp = [1] * 10
+    for i in range(n-1):
+        for j in range(1,10):
+            dp[j] += dp[j-1]
+
+    print(sum(dp)%10007)
+
