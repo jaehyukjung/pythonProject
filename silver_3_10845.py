52e27755d0f85a3a21ea20b7ce04b915ebad1b67
jaehyukjung <jhdool1594@gmail.com>
2023-03-03 20:56:15 +0900

    [03.03] 백준 10845번 문제(stdin) 써야함

---
+++ silver_3_10845.py
@@ -0,0 +1,37 @@
+from sys import stdin
+
+def queue(s):
+    s = s.split()
+
+    if s[0] == "push":
+        lst.append(int(s[1]))
+    elif s[0] == "pop":
+        if len(lst) == 0:
+            print(-1)
+        else:
+            print(lst.pop(0))
+    elif s[0] == "size":
+        print(len(lst))
+    elif s[0] == "empty":
+        if len(lst) == 0:
+            print(1)
+        else:
+            print(0)
+    elif s[0] == "front":
+        if len(lst) == 0:
+            print(-1)
+        else:
+            print(lst[0])
+    else:
+        if len(lst) == 0:
+            print(-1)
+        else:
+            print(lst[len(lst)-1])
+
+if __name__ == "__main__":
+
+    ea = int(stdin.readline())
+    lst = []
+    for i in range(ea):
+        s = stdin.readline()
+        queue(s)
