b0d10194236d3df55b2719aa7a7a61e2ac45e42f
jaehyukjung <jhdool1594@gmail.com>
2023-03-04 23:49:37 +0900

    [03.04] 백준 16933번

--- gold_1_16933.py
+++ gold_1_16933.py
@@ -3,44 +3,36 @@ from sys import stdin
 
 def bfs(si, sj):
     q = deque()
-    v = [[[0] * (k + 1) for x in range(m)] for _ in range(n)]
-    v[si][sj][0] = 1
-    q.append((si, sj, 0, 0))
-    lst = []
+    v = [[[MAX] * (k + 1) for x in range(m)] for _ in range(n)]
+    v[si][sj][k] = 0
+    q.append((si, sj, 1, k))
 
+    result = MAX
     while q:
-        ci, cj, bk, cnt = q.popleft()
+        ci, cj, t, bk = q.popleft()
         if ci == n - 1 and cj == m - 1:
-            lst.append(v[ci][cj][bk])
+            result = min(result, t)
             continue
-
+        day = t % 2
         for di, dj in ((-1, 0), (1, 0), (0, -1), (0, 1)):
             ni, nj = ci + di, cj + dj
             if 0 <= ni < n and 0 <= nj < m:
 
-                if arr[ni][nj] == "0":
-                    if (v[ni][nj][bk] == 0 or v[ni][nj][bk] > v[ci][cj][bk] + 1):
-                        v[ni][nj][bk] = v[ci][cj][bk] + 1
-                        q.append((ni, nj, bk, cnt + 1))
+                if arr[ni][nj] == "0" and v[ni][nj][bk] > t:
+                    v[ni][nj][bk] = t
+                    q.append((ni, nj, t+1,bk))
+
+                elif arr[ni][nj] == "1" and bk and v[ni][nj][bk-1] > t:
+                    if day:
+                        v[ni][nj][bk-1] = t
+                        q.append((ni,nj,t+1,bk-1))
+                    else:
+                        q.append((ci, cj, t+1, bk))
 
 
-                elif arr[ni][nj] == "1" and cnt % 2 == 0 and bk+1 <= k:
-                    if v[ni][nj][bk+1] == 0 or v[ni][nj][bk + 1] > v[ci][cj][bk] + 1:
-                        v[ni][nj][bk + 1] = v[ci][cj][bk] + 1
-                        q.append((ni, nj, bk + 1, cnt + 1))
-
-
-                elif arr[ni][nj] == "1" and cnt % 2 == 1:
-                    q.append((ci, cj, bk, cnt + 1))
-                    # if v[ni][nj][bk+1] == 0 or v[ni][nj][bk + 1] > v[ci][cj][bk] + 2:
-                    #     v[ni][nj][bk + 1] = v[ci][cj][bk] + 2
-                    #     q.append((ni, nj, bk + 1, cnt + 2))
-
-    if len(lst) == 0:
-        print(-1)
-    else:
-        print(min(lst))
+    print(result if result<MAX else -1)
 if __name__ == "__main__":
     n, m, k = map(int, stdin.readline().split())
     arr = [stdin.readline() for _ in range(n)]
+    MAX = 100000000000000000000000
     bfs(0, 0)
