422931a00497c930248f49d6a7f66edeab7e82cb
jaehyukjung <jhdool1594@gmail.com>
2023-03-21 23:50:15 +0900

    1

---
+++ silver_5_1789.py
@@ -0,0 +1,13 @@
+from sys import stdin
+
+if __name__ == "__main__":
+    n = stdin.readline()
+    s = n
+    l = n
+    s = n.replace('6','5')
+    a,b = map(int,s.split())
+    print(a+b, end=" ")
+    l = n.replace('5','6')
+    a, b = map(int, l.split())
+    print(a + b)
+    print(1)
